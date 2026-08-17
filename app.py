c1ca210314f50a1f38cf68c28652cfe81db77c44
Tassiana Bastos <tassianalb94@gmail.com>
2026-08-17 21:12:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.title("Ecossistemas de Carbono Irrecuperável na Amazônia")
+
+st.write(
+    "Aplicação em desenvolvimento para análise de dados "
+    "relacionados à conservação dos ecossistemas de carbono "
+    "irrecuperável e ao ODS 13."
+)
